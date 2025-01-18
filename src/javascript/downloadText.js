0661950b727ac3dc642d87ac70b35cd5f45573d2
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-18 19:26:25 +0800

    Add DownloadText File and Function

---
+++ src/javascript/downloadText.js
@@ -0,0 +1,8 @@
+function downloadText() {
+    const text = document.getElementById('output').textContent;
+    const blob = new Blob([text], { type: 'text/plain' });
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = 'transformed_text.txt';
+    a.click();
+}
