70a6ee69bd4949d679642f8b4045ee89f5c46d9b
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-18 19:41:48 +0800

    Fix: CopyToCliboard

--- src/javascript/copyToClipboard.js
+++ src/javascript/copyToClipboard.js
@@ -1,9 +1,7 @@
 function copyToClipboard(){
 
     const text = document.getElementById('output').textContent;
-    const blob = new Blob([text], {type: 'text/plain'});
-    const a = document.createElement('a');
-    a.href = URL.createObjectURL(blob);
-    a.download = 'transformed_text.txt';
-    a.click();
+    navigator.clipboard.writeText(text).then(() => {
+        alert('Text Copied to Clipboard');
+    });
 }
